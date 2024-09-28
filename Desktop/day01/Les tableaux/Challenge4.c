719190163e90ec8de04c849b02982abe53cef799
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-28 14:44:24 +0200

    adjust

---
+++ Desktop/day01/Les tableaux/Challenge4.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  int i, t_value, tie, max;
+    printf("Saisire la taille de tableau: ");
+    scanf("%d", &tie);
+
+  int t_tab[tie];
+  for( i = 0 ; i < tie ; i++){
+      printf("Elément %d : ", i+1);
+      scanf("%d",&t_tab[i]);
+  }
+   max = t_tab[0];
+            for(int i = 0; i < tie; i++){
+                if (t_tab[i]>max){
+                max = t_tab[i];    
+                }
+
+            }
+            printf("Le plus grand nombre c'est : %d ", max);
+  return 0;
+}
+  
