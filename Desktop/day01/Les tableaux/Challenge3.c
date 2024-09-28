2f10d3d1ab9cb08f0a2f8cd37ca978dabf835533
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-28 13:34:34 +0200

    adjust

---
+++ Desktop/day01/Les tableaux/Challenge3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+ int i, t_value, tie, e , sum=0 ;
+    printf("Saisire la taille de tableau: ");
+    scanf("%d", &tie);
+    
+int t_tab[tie];
+    for( i = 0 ; i < tie ; i++){
+        printf("Elément %d : ", i+1);
+        scanf("%d",&t_tab[i]);
+         sum = sum + t_tab[i];
+    }
+   
+    printf("la somme des éléments de tableau est %d",sum);
+    return 0;
+}
