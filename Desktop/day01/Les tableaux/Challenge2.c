6a57b82d9f3ad248aecbd25d51d3c7e60ce5ccba
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-28 13:04:14 +0200

    adjust

---
+++ Desktop/day01/Les tableaux/Challenge2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  int i, t_value, tie, e;
+    printf("Saisire la taille de tableau: ");
+    scanf("%d", &tie);
+
+  int t_tab[tie];
+  for( i = 0 ; i < tie ; i++){
+      printf("Elément %d : ", i+1);
+      scanf("%d",&t_tab[i]);
+  }
+   for( i = 0 ; i < tie ; i++){
+   printf("%d ",t_tab[i]);
+}
+ 
+    return 0;
+}
