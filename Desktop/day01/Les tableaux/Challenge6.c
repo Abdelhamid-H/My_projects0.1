7cd060b3838e284d22f4fce20891516d058c86ea
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-28 16:11:54 +0200

    adjust

--- Desktop/day01/Les tableaux/Challenge6.c
+++ Desktop/day01/Les tableaux/Challenge6.c
@@ -7,14 +7,13 @@ int main() {
     
     printf("Saisire le facteure de multiplication :");
     scanf("%d", &fm);
-int t_tab[tie];
+ int t_tab[tie];
     for( i = 0 ; i < tie ; i++){
         printf("Elément %d : ", i+1);
         scanf("%d",&t_tab[i]);
         
          mu = fm * t_tab[i];
+         printf("multiplication %d X %d = %d\n",t_tab[i], fm, mu);
     }
-   
-    printf("multiplication = %d",mu);
-    return 0;
+     return 0;
 }
