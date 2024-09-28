6a9a00f52dcac0a627c6ff9c591805b22ab5d68e
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-28 15:47:43 +0200

    adjust

---
+++ Desktop/day01/Les tableaux/Challenge6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+ int i, t_value, tie, mu=1, fm;
+    printf("Saisire la taille de tableau: ");
+    scanf("%d", &tie);
+    
+    printf("Saisire le facteure de multiplication :");
+    scanf("%d", &fm);
+int t_tab[tie];
+    for( i = 0 ; i < tie ; i++){
+        printf("Elément %d : ", i+1);
+        scanf("%d",&t_tab[i]);
+        
+         mu = fm * t_tab[i];
+    }
+   
+    printf("multiplication = %d",mu);
+    return 0;
+}
