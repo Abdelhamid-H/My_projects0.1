e39eccbab3035da73c68778e2d0b603482ee642f
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-30 09:47:01 +0200

    adjust

---
+++ Desktop/day01/Les fonctions/Challenge2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+   int mult( int a , int b){
+    
+   return a * b;
+   }
+   int main() {
+     int prod;
+     prod = mult(10 , 5);
+     printf ("%d" ,prod);
+    return 0;
+}
