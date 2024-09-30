30c58e048ce3aba3e2f1fbac824b48d22b380689
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-30 09:33:39 +0200

    adjust

---
+++ Desktop/day01/Les fonctions/Challenge1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+  
+  int add(int a,int b){
+      
+  return a + b;
+  }
+
+  int main() {
+     int som;
+     som = add(20 , 40);
+     printf("%d", som);
+    
+    return 0;
+}
