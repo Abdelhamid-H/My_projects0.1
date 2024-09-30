6ae31181ba8652d86431b097c736ce640567a2d2
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-30 11:44:23 +0200

    adjust

---
+++ Desktop/day01/Les fonctions/Challenge4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int min(int a, int b){
+  if (a < b){
+    return a;
+  }
+  else {
+    return b;
+  }
+
+}
+int main() {
+    int petit;
+    petit = min(100,150);
+    printf("%d", petit);
+    
+    return 0;
+}
