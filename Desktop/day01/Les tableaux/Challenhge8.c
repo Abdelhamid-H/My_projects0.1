36aa1aba2972a414b9ed549d3ce90eebab757ce2
Abdelhamid-H <riad.mouslik@gmail.com>
2024-09-30 09:04:31 +0200

    adjust

---
+++ Desktop/day01/Les tableaux/Challenhge8.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int i;
+  int t[5] = {2,4,5,8,9};
+  int tt[i];
+  
+  tt[i]=t[i];
+  
+  for(i=0; i<5; i++){
+      printf("%d", tt[i]);
+  }
+    
+     return 0;
+}
