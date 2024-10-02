2b634cb79da8b8c3b651ffc4065e7787a4271a96
Abdelhamid-H <riad.mouslik@gmail.com>
2024-10-02 12:44:40 +0200

    adjust

--- Desktop/day01/Les tableaux/Challenhge8.c
+++ Desktop/day01/Les tableaux/Challenhge8.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   int i;
-  int t[5] = {2,4,5,8,9};
+  int t[5] = {2, 4, 5, 8, 9};
   int tt[i];
-  
-  tt[i]=t[i];
-  
-  for(i=0; i<5; i++){
-      printf("%d", tt[i]);
+
+  tt[i] = t[i];
+
+  for (i = 0; i < 5; i++)
+  {
+    printf("%d", tt[i]);
   }
-    
-     return 0;
+
+  return 0;
 }
